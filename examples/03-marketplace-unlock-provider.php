00894730d5094a1013b7071543c3c0f072dbd0d6
Niels Theen <n.theen@onoffice.de>
2020-02-27 15:03:05 +0100

    Add example for marketplace to unlock a provider

---
+++ examples/03-marketplace-unlock-provider.php
@@ -0,0 +1,22 @@
+<?php
+
+include __DIR__ . '/../vendor/autoload.php';
+
+use onOffice\SDK\onOfficeSDK;
+
+$pSDK = new onOfficeSDK();
+$pSDK->setApiVersion('latest');
+
+$parameterCacheId = '<insert parameterCacheId from IFrame url>';
+
+$parameterUnlockProvider = ['parameterCacheId' => $parameterCacheId];
+
+$handleUnlockProvider = $pSDK->callGeneric(
+	onOfficeSDK::ACTION_ID_DO,
+	'unlockProvider',
+	$parameterUnlockProvider
+);
+
+$pSDK->sendRequests('put the token here', 'and secret here');
+
+var_export($pSDK->getResponseArray($handleUnlockProvider));
